b02ff3a76fe1257b0c4dfc06dda1a0d67e9a9387
Prasad Motale <motaleprasad@gmail.com>
2021-01-04 16:02:38 +0530

    Tut 7 done

--- api/views.py
+++ api/views.py
@@ -10,6 +10,21 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self,request,fromat=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) >0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data,status=status.HTTP_200_OK)
+            return Response({'Room Not Found' : 'INVALID ROOM CODE'},status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request':'Code Paramater not found in request'},status=status.HTTP_400_BAD_REQUEST)
+
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import Room from './Room';
 import { BrowserRouter as Router, Switch,Redirect,Route,Link } from "react-router-dom";
 
 export default class HomePage extends Component{
@@ -17,6 +18,7 @@ export default class HomePage extends Component{
                         </Route>
                         <Route path='/join' component={RoomJoinPage}/>
                         <Route path='/create' component={CreateRoomPage}/>
+                        <Route path='/room/:roomCode' component={Room}/>
                     </Switch>
                 </Router>
             </div>
