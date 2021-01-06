252a4519b9e917c206bc7fe46289f5c2a5b7e396
Prasad Motale <motaleprasad@gmail.com>
2021-01-06 13:17:59 +0530

    Tut 12 done

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -9,18 +9,31 @@ import { Link } from "react-router-dom";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Collapse } from '@material-ui/core';
+import {Alert} from '@material-ui/lab';
 
 export default class CreateRoomPage extends Component{
-    defaultVotes=2;
+    static defaultProps = {
+        guestCanPause: true,
+        votesToSkip:2,
+        update: false,
+        roomCode:null,
+        updateCallback:()=>{},
+        errorMsg:"",
+        successMsg:"",
+
+    }
     constructor(props){
         super(props);
         this.state={
-            guestCanPause:true,
-            votesToSkip:this.defaultVotes
+            guestCanPause:this.props.guestCanPause,
+            votesToSkip:this.props.votesToSkip,
+            
         };
         this.handleRoomCreatePressed=this.handleRoomCreatePressed.bind(this)
         this.handleVotesChange=this.handleVotesChange.bind(this)
         this.handleGuestCanPauseChange=this.handleGuestCanPauseChange.bind(this)
+        this.handleUpdateButtonPressed = this.handleUpdateButtonPressed.bind(this);
     }
     handleVotesChange(e){
         this.setState({
@@ -42,16 +55,89 @@ export default class CreateRoomPage extends Component{
                 guest_can_pause:this.state.guestCanPause
             })
         };
-        fetch('api/create-room',requestOptions).then((response)=>
+        fetch('/api/create-room',requestOptions).then((response)=>
         response.json()
         ).then((data)=>this.props.history.push("/room/"+data.code));  
     }
+
+    handleUpdateButtonPressed() {
+        const requestOptions = {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            votes_to_skip: this.state.votesToSkip,
+            guest_can_pause: this.state.guestCanPause,
+            code: this.props.roomCode,
+          }),
+        };
+        fetch("/api/update-room", requestOptions).then((response) => {
+          if (response.ok) {
+            this.setState({
+              successMsg: "Room updated successfully!",
+            });
+          } else {
+            this.setState({
+              errorMsg: "Error updating room...",
+            });
+          }
+          this.props.updateCallback();
+        });
+      }
+    
+
+    renderCreateButtons(){
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" onClick={this.handleRoomCreatePressed}>
+                        Create A Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    renderUpdateButtons(){
+        return(
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" onClick={this.handleUpdateButtonPressed}>
+                        Update Room
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render(){
+        const title = this.props.update ? "Update Room" : "Create a Room"
         return(
             <Grid container spacing = {1} >
                 <Grid item xs={12} align="center">
+                    <Collapse in={this.state.errorMsg != "" || this.state.successMsg != ""}>
+                        {this.state.successMsg != "" ? 
+                            (<Alert severity="success" onClose={()=>{this.setState({successMsg:""})
+                            }}
+                            >
+                                {this.state.successMsg}
+                            </Alert>)
+                                :
+                            (<Alert severity="error" onClose={()=>{this.setState({errorMsg:""})
+                            }}
+                            >
+                                {this.state.errorMsg}
+                            </Alert>)
+                        }
+                    </Collapse>
+                </Grid>
+                <Grid item xs={12} align="center">
                     <Typography component="h4" variant="h4">
-                        Create a Room
+                        {title}
                     </Typography>
                 </Grid>
                 <Grid item xs={12} align="center">
@@ -61,7 +147,7 @@ export default class CreateRoomPage extends Component{
                                 Guest Control of Playback State
                             </div>
                         </FormHelperText>
-                        <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange} >
+                        <RadioGroup row defaultValue={this.props.guestCanPause.toString()} onChange={this.handleGuestCanPauseChange} >
                             <FormControlLabel value="true" 
                                 control ={<Radio color={"primary"}/>} 
                                 label ="Play/Pause"
@@ -80,7 +166,7 @@ export default class CreateRoomPage extends Component{
                         <TextField required="true" 
                             onChange={this.handleVotesChange}
                             type ="number" 
-                            defaultValue={this.defaultVotes}
+                            defaultValue={this.state.votesToSkip}
                             inputProps={{
                                 min:1,
                                 style:{textAlign:"center"},
@@ -88,18 +174,13 @@ export default class CreateRoomPage extends Component{
                             />
                             <FormHelperText>
                                 <div align="center">
-                                    Voted Required to Skip Song
+                                    Votes Required to Skip Song
                                 </div>
                             </FormHelperText>
                              
                     </FormControl>
                 </Grid>
-                <Grid item xs={12} align="center">
-                    <Button color="primary" variant="contained" onClick={this.handleRoomCreatePressed}>Create A Room</Button>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Button color="secondary" variant="contained" to="/" component={Link}>Back</Button>
-                </Grid>
+                {this.props.update ? this.renderUpdateButtons() : this.renderCreateButtons()}
             </Grid>
         );
     }
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,63 +1,88 @@
-import React, { Component } from 'react';
-import CreateRoomPage from "./CreateRoomPage";
+import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
-import Room from './Room';
-import { BrowserRouter as Router, Switch,Redirect,Route,Link } from "react-router-dom";
-import { Button,ButtonGroup,Grid,Typography } from '@material-ui/core';
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-export default class HomePage extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            roomCode:null
-        }
-    }
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: null,
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
+  }
 
-    async componentDidMount(){
-        fetch('/api/user-in-room')
-        .then((response)=>response.json())
-        .then((data)=> {
-            this.setState({
-                roomCode: data.code
-            });
-        }); 
-    }
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
 
-    renderHomePage(){
-        return(
-            <Grid container spacing={3}>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h3" compact="h3">Spotify Party</Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <ButtonGroup variant="contained" color="primary">
-                        <Button color="primary" to="/join" component={Link}>Join a Room</Button>
-                        <Button color="secondary" to="/create" component={Link}>Create a Room</Button>
-                    </ButtonGroup>
-                </Grid>
-            </Grid>
-        );
-    }
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
 
-    render(){
-        return(
-            <div>
-                <Router>
-                    <Switch>
-                        <Route exact path='/'
-                        render={()=>{
-                            return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}/>)
-                                : (
-                                    this.renderHomePage()
-                                );
-                        }}
-                        />
-                        <Route path='/join' component={RoomJoinPage}/>
-                        <Route path='/create' component={CreateRoomPage}/>
-                        <Route path='/room/:roomCode' component={Room}/>
-                    </Switch>
-                </Router>
-            </div>
-        );
-    }
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
